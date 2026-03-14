f789da5a406d3cc0b62a1cd1550c3f19abf7993a
rishirajsinh <rishirajsinh.rathod.cg@gmail.com>
2026-03-14 12:22:17 +0530

    Project Setup: Configure React, Vite, and Tailwind CSS base

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-white flex flex-col">
+      <main className="flex flex-col gap-20 lg:gap-24">
+        {/* Components will go here */}
+      </main>
+    </div>
+  )
+}
+
+export default App
